3df2fba748e940ecab64035088220bfbd5c88692
edwin yue <53625790+edwaddle@users.noreply.github.com>
2024-07-27 00:57:49 -0700

    Finished the rest of the project
    Used the previous button I was messing with, and followed instructions on Odin Project to create a Rock Paper Scissors game. Uses constants to keep track of what the user or computer is playing, and debugged with console.log in order to make sure everything worked. Mostly used if else statements, but may impliment switch statements if possible. Utilized the messaging to set up computer score, human score, and tie counter. Also used CSS to format it a little nicer.

--- script.js
+++ script.js
@@ -1,6 +1,80 @@
 console.log("Hello Workd");
 const myMessage = document.querySelector('.message');
 console.log(myMessage); 
+const ROCK_VALUE = 1;
+const PAPER_VALUE = 2;
+const SCISSOR_VALUE = 3;
+let humanScore = 0; computerScore = 0; tieCounter = 0;
 function myFunction(){
-    myMessage.textContent = String(Math.floor(Math.random()*100));
+    humanScore = 0; 
+    computerScore = 0;
+    tieCounter = 0;
+    playGame();
+}
+function getComputerChoice(){
+    let num = Math.floor(Math.random()*3)+1;
+    console.log(num);
+    return num;
+    //1 is rock
+    //2 is paper
+    //3 is scissors
+}
+
+function getHumanChoice(){
+    let choice = prompt("Choose Rock, Paper, or Scissors");
+    choice = choice.toLowerCase();
+    if (choice === "rock"){
+        return ROCK_VALUE;
+    }
+    else if (choice === "paper"){
+        return PAPER_VALUE;
+    }
+    else if (choice === "scissors"){
+        return SCISSOR_VALUE;
+    }
+    else{
+        return -1;
+    }
+}
+
+function playRound(humanChoice, computerChoice){
+    if (humanChoice === computerChoice){
+        tieCounter++;
+    }
+    else if (humanChoice === ROCK_VALUE){
+        if (computerChoice === SCISSOR_VALUE){
+            ++humanScore;
+        }
+        else if (computerChoice === PAPER_VALUE){
+            ++computerScore;
+        }
+    }
+    else if (humanChoice === PAPER_VALUE){
+        if (computerChoice === ROCK_VALUE){
+            ++humanScore;
+        }
+        else if (computerChoice === SCISSOR_VALUE){
+            ++computerScore;
+        }
+    }
+    else if (humanChoice === SCISSOR_VALUE){
+        if (computerChoice === ROCK_VALUE){
+            ++computerScore;
+        }
+        else if (computerChoice === PAPER_VALUE){
+            ++humanChoice;
+        }
+    }
+    console.log( humanScore + " " + computerScore + ". " + humanChoice + " " + computerChoice);
+    myMessage.textContent = "Human Score is: " + humanScore + ". Computer Score is: " + computerScore + ". Tie Counter is: " + tieCounter + "." ;
+}
+
+function playGame(){
+    for (i = 0; i < 5; i++){
+        setTimeout(function(){
+            playRound(getHumanChoice(), getComputerChoice());
+        }, 1000);
+        
+        
+    }
 }
