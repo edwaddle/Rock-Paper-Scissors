aa9cc6ce788263ef7a638bc0bdd971f79666a88d
edwin yue <53625790+edwaddle@users.noreply.github.com>
2024-07-26 23:23:54 -0700

    made a random number generator
    Used to make sure that js and html are linked, and overall used just to explore some things that I wanted to try before actually heading into the project

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 console.log("Hello Workd");
+const myMessage = document.querySelector('.message');
+console.log(myMessage); 
+function myFunction(){
+    myMessage.textContent = String(Math.floor(Math.random()*100));
+}
